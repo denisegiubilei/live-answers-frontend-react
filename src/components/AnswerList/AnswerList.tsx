6a28e6f8e3b569d8a13a4ef6c02b54ce4a660d15
Denise Giubilei <giubileidenise@gmail.com>
2021-09-06 17:41:46 -0300

    refact: move socket logic to AnswerListPage and receive answers as props

--- src/components/AnswerList/AnswerList.tsx
+++ src/components/AnswerList/AnswerList.tsx
@@ -1,44 +1,27 @@
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { AnswerEntity } from '../../interfaces/Answer';
-import { listAnswers } from '../../api/answer';
-import { SocketContext, SocketListenEvents, UpdateAnswersArgs } from '../../context/SocketContext';
+interface AnswerListProps {
+  answers: AnswerEntity[];
+}
 
-const AnswerList = () => {
-  const { socket, isConnected }= useContext(SocketContext);
-
-  const [answers, setAnswers] = useState([] as AnswerEntity[]);
-
-  useEffect(() => {
-    listAnswers().then(res => {
-      setAnswers(res);
-    });
-  }, []);
+const AnswerList = ({ answers }: AnswerListProps) => {
+  const [answerList, setAnswerList] = useState(answers);
 
   useEffect(() => {
-    if (isConnected) {
-      socket.on(
-        SocketListenEvents.UPDATE_ANSWERS_LIST,
-        ({ answersList }: UpdateAnswersArgs) => {
-          setAnswers(answersList);
-        }
-      );
-    }
-  }, [socket, isConnected]);
+    setAnswerList(answers);
+  }, [answers]);
 
   return (
-    <section>
-      <h2>Live Answers</h2>
-      <ul>
-        {
-          answers?.map(({ answers_id, answers_text }: AnswerEntity) => (
-            <li key={answers_id}>
-              <span>{answers_text}</span>
-            </li>
-          ))
-        }
-      </ul>
-    </section >
+    <ul>
+      {
+        answerList?.map(({ answers_id, answers_text }: AnswerEntity) => (
+          <li key={answers_id}>
+            <span>{answers_text}</span>
+          </li>
+        ))
+      }
+    </ul>
   );
 };
 
--- src/pages/AnswersList/AnswersListPage.tsx
+++ src/pages/AnswersList/AnswersListPage.tsx
@@ -1,7 +1,40 @@
+import { useContext, useEffect, useState } from "react";
+
+import { AnswerEntity } from "../../interfaces/Answer";
+import { SocketContext, SocketListenEvents, UpdateAnswersArgs } from "../../context/SocketContext";
+import { listAnswers } from "../../api/answer";
+
 import { AnswerList } from "../../components/AnswerList/AnswerList";
+import { Header } from "../../components/Header/Header";
 
 const AnswersListPage = () => {
-  return <AnswerList />;
+  const { socket, isConnected } = useContext(SocketContext);
+
+  const [answers, setAnswers] = useState([] as AnswerEntity[]);
+
+  useEffect(() => {
+    listAnswers().then(res => {
+      setAnswers(res);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isConnected) {
+      socket.on(
+        SocketListenEvents.UPDATE_ANSWERS_LIST,
+        ({ answersList }: UpdateAnswersArgs) => {
+          setAnswers(answersList);
+        }
+      );
+    }
+  }, [socket, isConnected]);
+
+  return (
+    <>
+      <Header title="Live answers" />
+      <AnswerList answers={answers} />
+    </>
+  );
 };
 
 export default AnswersListPage;
